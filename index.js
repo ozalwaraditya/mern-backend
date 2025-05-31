270e1f79ed97cf9bf5eca69f26899fb253cd96b4
ozalwaraditya <adityaozalwar03@gmail.com>
2025-05-31 08:48:57 +0530

    updated two file

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-
-dotenv.config();
 
 const app = express();
 
 const allowedOrigin = ['https://mern-frontend-six-tan.vercel.app', 'http://localhost:5173'];
 
 app.use(cors({ origin: allowedOrigin }));
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -19,12 +15,5 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res) => {
   res.json({ success: true, message: '✅ Test route from Express is working!' });
 });
-
-if (process.argv[1].endsWith('index.js')) {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on - ${PORT}`);
-  });
-}
 
 export default app;
